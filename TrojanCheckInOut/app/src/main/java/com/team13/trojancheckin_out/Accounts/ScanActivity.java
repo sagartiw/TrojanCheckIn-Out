682c4071ce84d8fe6579a0ce79baf28513971f7b
Annika Oeth <aoeth@usc.edu>
2021-03-06 14:23:57 -0800

    added files with functionality to scan qr codes, but there are some errors I cant fix ugh

---
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Accounts/ScanActivity.java
@@ -0,0 +1,87 @@
+package com.team13.trojancheckin_out.Accounts;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+
+public class ScanActivity extends AppCompatActivity {
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+    private TextView textView;
+    private BarcodeDetector barcodeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        surfaceView = findViewById(R.id.camera);
+        textView = findViewById(R.id.text);
+
+        barcodeDetector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+        cameraSource = new CameraSource.Builder(getApplicationContext(), barcodeDetector)
+                .setRequestedPreviewSize(640, 480).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrcode = detections.getDetectedItems();
+                if (qrcode.size() != 0) {
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(qrcode.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
