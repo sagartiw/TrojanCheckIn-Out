b633cad7946a22a3d184ca9f7092a633aeb7eeab
mindydiep <mindydie@usc.edu>
2021-03-10 15:21:42 -0800

    startup on emulator

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/Register.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/Register.java
@@ -2,15 +2,29 @@ package com.team13.trojancheckin_out.Layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.team13.trojancheckin_out.R;
 
 public class Register extends AppCompatActivity {
 
+    private Button Register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Register = (Button)findViewById(R.id.register2);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
