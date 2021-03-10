a4a66704f5913f6ab9b3f6d412a049a31a5c5601
mindydiep <mindydie@usc.edu>
2021-03-09 21:30:25 -0800

    startup page

---
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/Register.java
@@ -0,0 +1,16 @@
+package com.team13.trojancheckin_out.Layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.team13.trojancheckin_out.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
