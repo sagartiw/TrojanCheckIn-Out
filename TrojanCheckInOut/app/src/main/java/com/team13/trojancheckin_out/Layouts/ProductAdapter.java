dd632f9cfca01ff8f5f6042088441885817c41d5
mindydiep <mindydie@usc.edu>
2021-03-11 19:20:38 -0800

    finish managerlanding view + start qr popup

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/ProductAdapter.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/ProductAdapter.java
@@ -1,11 +1,18 @@
-package com.team13.trojancheckin_out.UPC;
+package com.team13.trojancheckin_out.Layouts;
 
 import android.content.Context;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -13,6 +20,9 @@ import android.widget.TextView;
 import com.team13.trojancheckin_out.R;
 
 import java.util.List;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
 
 /**
  * Created by Belal on 10/18/2017.
@@ -24,6 +34,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
     //this context we will use to inflate the layout
     private Context mCtx;
+    private ImageButton qrButton;
 
     //we are storing all the products in a list
     private List<Product> productList;
@@ -53,6 +64,36 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         holder.textViewCapacity.setText(String.valueOf(product.getCapacity()));
         holder.textViewPercent.setText(String.valueOf(product.getPercent()) + "%");
         holder.progressBar.setProgress((product.getPercent()));
+
+        qrButton = holder.imageButton;
+
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // inflate the layout of the popup window
+                LayoutInflater inflater = LayoutInflater.from(mCtx);
+                View popupView = inflater.inflate(R.layout.qr_popup, null);
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window token
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+            }
+        });
     }
 
 
@@ -65,6 +106,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     class ProductViewHolder extends RecyclerView.ViewHolder {
 
         TextView textViewTitle, textViewCurrent, textViewCapacity, textViewPercent;
+        ImageButton imageButton;
         ProgressBar progressBar;
 
         public ProductViewHolder(View itemView) {
@@ -75,6 +117,8 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             textViewCapacity = itemView.findViewById(R.id.textViewCapacity);
             textViewPercent = itemView.findViewById(R.id.textViewPercent);
             progressBar = itemView.findViewById(R.id.progressBar);
+            imageButton = itemView.findViewById(R.id.imageButton4);
         }
     }
+
 }
