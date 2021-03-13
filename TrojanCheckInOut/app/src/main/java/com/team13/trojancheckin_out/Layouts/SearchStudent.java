2ea2b9f63ad890d8465c7920714192e0080df822
KabirSamra <ksamra@usc.edu>
2021-03-12 19:20:59 -0800

    edits so far

---
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/SearchStudent.java
@@ -0,0 +1,70 @@
+package com.team13.trojancheckin_out.Layouts;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.team13.trojancheckin_out.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class SearchStudent extends AppCompatActivity {
+
+    private Button Search;
+    private Button Back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_student);
+
+        Search = (Button)findViewById(R.id.button7);
+
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Back = (Button)findViewById(R.id.back3);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchStudent.this, ManagerLanding.class);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final PopupMenu menu = new PopupMenu(this, fab);
+        menu.getMenu().add("Student View");
+        menu.getMenu().add("Sign Out");
+        menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                // insert your code here
+                Log.d("menu title: ", item.getTitle().toString());
+                if(item.getTitle().toString().equals("Student View")){
+                    Intent intent = new Intent(SearchStudent.this, StudentLanding.class);
+                    startActivity(intent);
+                }
+                return true; }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menu.show();
+            }
+        });
+    }
+}
