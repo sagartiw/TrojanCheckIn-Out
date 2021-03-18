1c6a363bb238e827e17f1f4a580b7b75ed0b7624
Arian Memari <memari@usc.edu>
2021-03-18 13:41:11 -0700

    Sagar check

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/Register.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/Register.java
@@ -6,13 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.team13.trojancheckin_out.Accounts.R;
+import com.team13.trojancheckin_out.Database.AccountManipulator;
 
 public class Register extends AppCompatActivity {
 
     private Button Register;
     private Button Back;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,9 @@ public class Register extends AppCompatActivity {
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Category");
+                reference.setValue("This is my set value");
                 Intent intent = new Intent(Register.this, CompleteProfile.class);
                 startActivity(intent);
             }
