ff43faaf47f1a6f7538abdb93e4d782f09bfd345
sagartiw <sagartiw@usc.edu>
2021-03-21 12:11:17 -0700

    asdf

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/Login.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/Login.java
@@ -6,13 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.team13.trojancheckin_out.Accounts.R;
+import com.team13.trojancheckin_out.Accounts.User;
+import com.team13.trojancheckin_out.Database.AccountManipulator;
 
 public class Login extends AppCompatActivity {
 
     private Button Login;
     private Button Back;
+
+    private AccountManipulator accountManipulator = new AccountManipulator();
+    private User user;
+    private EditText email, password;
+    private FirebaseAuth fauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,11 @@ public class Login extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                email = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+                password = (EditText) findViewById(R.id.editTextTextPassword3);
+
+
                 Intent intent = new Intent(Login.this, ManagerLanding.class);
                 startActivity(intent);
             }
