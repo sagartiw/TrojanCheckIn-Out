f2a50f919142b2eb06301edebe4bef1eb4bdfc3d
KabirSamra <ksamra@usc.edu>
2021-03-22 09:09:43 -0700

    LAST COMMIT BEFORE SUBMISSION

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Database/BuildingManipulator.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Database/BuildingManipulator.java
@@ -57,6 +57,7 @@ public class BuildingManipulator {
      */
     public Building getBuilding(String acronym) {
         System.out.println("IM HERE: " + acronym);
+        if (currentBuildings == null ) return new Building();
         System.out.println("BITCH : " + currentBuildings.get(acronym).getAbbreviation());
         return currentBuildings.get(acronym);
     }
