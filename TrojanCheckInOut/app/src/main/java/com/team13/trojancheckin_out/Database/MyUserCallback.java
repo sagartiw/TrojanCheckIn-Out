2b4ed89c33bfefa710ea9c24307cadf7f8664817
Arian Memari <memari@usc.edu>
2021-03-21 20:38:14 -0700

    m

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Database/MyUserCallback.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Database/MyUserCallback.java
@@ -4,6 +4,6 @@ import com.team13.trojancheckin_out.Accounts.User;
 
 import java.util.Map;
 
-public interface MyCallback {
+public interface MyUserCallback {
     void onCallback(Map<String, User> map);
 }
