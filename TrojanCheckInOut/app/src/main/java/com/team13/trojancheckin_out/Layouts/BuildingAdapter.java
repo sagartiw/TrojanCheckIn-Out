a0184c21e75f53cad0388e0b09beea13b8613044
KabirSamra <ksamra@usc.edu>
2021-03-22 02:03:09 -0700

    updartes

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/BuildingAdapter.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/BuildingAdapter.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -101,6 +102,9 @@ public class BuildingAdapter extends RecyclerView.Adapter<BuildingAdapter.Buildi
                 LayoutInflater inflater = LayoutInflater.from(mCtx);
                 View popupView = inflater.inflate(R.layout.cap_popup, null);
                 Button closeButton = (Button) popupView.findViewById(R.id.button6);
+                Button submitButton = (Button) popupView.findViewById(R.id.button9);
+                TextView name = (TextView) popupView.findViewById(R.id.textView18);
+                name.setText(building.getName());
 
                 // create the popup window
                 int width = LinearLayout.LayoutParams.WRAP_CONTENT;
@@ -114,10 +118,23 @@ public class BuildingAdapter extends RecyclerView.Adapter<BuildingAdapter.Buildi
                 // which view you pass in doesn't matter, it is only used for the window token
                 popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
+
+
                 // dismiss the popup window when touched
                 closeButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+                submitButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        EditText num = (EditText) popupView.findViewById(R.id.editTextNumber2);
+                        String w = num.toString();
+                        building.setCapacity(Integer.parseInt(w));
                         popupWindow.dismiss();
                     }
                 });
