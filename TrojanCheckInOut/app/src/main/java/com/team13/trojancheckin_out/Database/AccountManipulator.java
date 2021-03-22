f42a12aec789217a324b276bbf9804bcef96a2af
sagartiw <sagartiw@usc.edu>
2021-03-21 21:47:31 -0700

    asdf

--- TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Database/AccountManipulator.java
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Database/AccountManipulator.java
@@ -21,22 +21,14 @@ public class AccountManipulator extends User {
     public static final FirebaseDatabase rootNode = FirebaseDatabase.getInstance();
     public static final DatabaseReference referenceUsers = rootNode.getReference("Users");
 
-<<<<<<< HEAD
-    private Map<String, User> studentAccounts = new HashMap<>();
-    private Map<String, User> managerAccounts = new HashMap<>();
-=======
     private static Map<String, User> studentAccounts;
     private static Map<String, User> managerAccounts;
     private static Map<String, User> allAccounts;
->>>>>>> 7fc335daec752a962f124b432cc9e8e45eb2e024
 
     /**
      * @return the current list of registered student accounts. Accesses the Google Firebase to
      * parse the JSON data into Java "User" objects and into the studentAccounts data structure.
      */
-<<<<<<< HEAD
-    public Map<String, User> getStudentAccounts() {
-=======
     public void getAllAccounts(MyUserCallback myUserCallback) {
         allAccounts = new HashMap<>();
 
@@ -64,7 +56,6 @@ public class AccountManipulator extends User {
         studentAccounts = new HashMap<>();
         managerAccounts = new HashMap<>();
 
->>>>>>> 7fc335daec752a962f124b432cc9e8e45eb2e024
         referenceUsers.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
@@ -75,11 +66,7 @@ public class AccountManipulator extends User {
                         }
                     }
 
-<<<<<<< HEAD
-                    System.out.println("before leaving datachange");
-=======
                     myUserCallback.onCallback(studentAccounts);
->>>>>>> 7fc335daec752a962f124b432cc9e8e45eb2e024
                 }
 
                 @Override
@@ -87,33 +74,12 @@ public class AccountManipulator extends User {
 
 
         });
-<<<<<<< HEAD
-        System.out.println("BEFORE RETURN");
-        for (User u : studentAccounts.values()) {
-            System.out.println("My name: " + u.getName());
-        }
-        return studentAccounts;
 
-=======
->>>>>>> 7fc335daec752a962f124b432cc9e8e45eb2e024
     }
 
     /**
      * @return the current list of registered student accounts. Same concept as getStudentAccounts.
      */
-<<<<<<< HEAD
-    public Map<String, User> getManagerAccounts() {
-        referenceUsers.addValueEventListener(
-                new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                            User user = ds.getValue(User.class);
-                            if (user.isManager().equalsIgnoreCase("true")) {
-                                managerAccounts.put(user.getId(), user);
-                            }
-                        }
-=======
     public void getManagerAccounts(MyUserCallback myUserCallback) {
         studentAccounts = new HashMap<>();
         managerAccounts = new HashMap<>();
@@ -125,20 +91,15 @@ public class AccountManipulator extends User {
                     User user = ds.getValue(User.class);
                     if (user.isManager().equalsIgnoreCase("true")) {
                         studentAccounts.put(user.getId(), user);
->>>>>>> 7fc335daec752a962f124b432cc9e8e45eb2e024
                     }
                 }
 
                 myUserCallback.onCallback(studentAccounts);
             }
 
-<<<<<<< HEAD
-        return managerAccounts;
-=======
             @Override
             public void onCancelled(DatabaseError databaseError) { }
         });
->>>>>>> 7fc335daec752a962f124b432cc9e8e45eb2e024
     }
 
     /**
@@ -154,19 +115,6 @@ public class AccountManipulator extends User {
      */
     public Boolean createAccount(User user) {
         referenceUsers.child(user.getId()).setValue(user);
-<<<<<<< HEAD
-        if (user.isManager().equalsIgnoreCase("true")) {
-            managerAccounts.put(user.getId(), user);
-        } else {
-            studentAccounts.put(user.getId(), user);
-        }
-=======
-//        if (user.isManager().equalsIgnoreCase("true")) {
-//            managerAccounts.put(user.getId(), user);
-//        } else {
-//            studentAccounts.put(user.getId(), user);
-//        }
->>>>>>> 7fc335daec752a962f124b432cc9e8e45eb2e024
         return true;
     }
 
@@ -189,36 +137,6 @@ public class AccountManipulator extends User {
      */
     public Boolean login() {
         //gets authorization
-        @Override
-        public void onStart() {
-            super.onStart();
-            // Check if user is signed in (non-null) and update UI accordingly.
-            FirebaseUser currentUser = rootNode.getCurrentUser();
-            if(currentUser != null){
-                reload();
-            }
-        }
-
-
-        rootNode.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // nformation
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = rootNode.getCurrentUser();
-                            updateUI(user);
-                        }
-                        else {
-                            //sign in failed
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
         return true;
     }
 
