c3e1c3dce2e2c11dd27460a66c37ee8142a90161
Liza Moody <emoody110@gmail.com>
2021-03-28 22:57:43 -0700

    added building tests

--- TrojanCheckInOut/app/src/androidTest/java/com/team13/trojancheckin_out/Accounts/ManagerTest.java
+++ TrojanCheckInOut/app/src/androidTest/java/com/team13/trojancheckin_out/Accounts/ManagerTest.java
@@ -4,6 +4,19 @@ import junit.framework.TestCase;
 
 public class ManagerTest extends TestCase {
 
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testIsManager() {
+    }
+
+    public void testSetManager() {
+    }
+
     public void testImportCSV() {
     }
 
--- TrojanCheckInOut/app/src/test/java/com/team13/trojancheckin_out/UPC/BuildingTest.java
+++ TrojanCheckInOut/app/src/test/java/com/team13/trojancheckin_out/UPC/BuildingTest.java
@@ -1,8 +1,12 @@
 package com.team13.trojancheckin_out.UPC;
 
 import junit.framework.TestCase;
+import com.team13.trojancheckin_out.Accounts.User;
+
 
 public class BuildingTest extends TestCase {
+    public Building b = new Building("Salvatori", "", 50, "salQR");
+
 
     public void setUp() throws Exception {
         super.setUp();
@@ -11,31 +15,60 @@ public class BuildingTest extends TestCase {
     public void tearDown() throws Exception {
     }
 
-    public void testGetAbbreviation() {
+    public void testAbbreviation() {
+        String abrev = "GFS";
+        b.setAbbreviation(abrev);
+        assertEquals(abrev, b.getAbbreviation());
     }
 
-    public void testSetAbbreviation() {
+    public void testGetSetName() {
+        String name = "Salvatori Hall";
+        b.setName(name);
+        assertEquals(name, b.getName());
+        name = "Grace Ford Salvatori Hall";
+        b.setName(name);
+        assertEquals(name, b.getName());
     }
 
-    public void testTestGetName() {
-    }
 
-    public void testTestSetName() {
-    }
-
-    public void testGetCapacity() {
+    public void testGetSetCapacity() {
+        b.setCapacity(-1);
+        assertEquals(0, b.getCapacity());
+        b.setCapacity(50);
+        assertEquals(50, b.getCapacity());
     }
 
     public void testSetCapacity() {
+        int cap = 100;
+        b.setCapacity(cap);
+        assertEquals(cap, b.getCapacity());
     }
 
     public void testGetQRCode() {
+        b.setQRCode("gfsQR");
+        assertEquals("gfsQR", b.getQRCode());
+       // b.setQRCode(1);
+       // assertEquals(1, b.getQRCode());
     }
 
     public void testSetQRCode() {
+        String code = "setQR";
+        b.setQRCode(code);
+        assertEquals(code, b.getQRCode());
     }
 
     public void testGetCurrentStudents() {
+
+        User a = new User("will", "w@usc.edu", "123", "user/path", "1234567890",
+        true, b, null, "meche", "true");
+        User c = new User("cara", "c@usc.edu", "123", "user/path", "1234567890",
+                false, b, null, "meche", "false");
+       // b.addStudent(a);
+      //  b.addStudent(c);
+       // assertEquals(2, b.getCurrentCount());
+        assert(b.isInBuilding(a));
+
+
     }
 
     public void testSetStudents() {
@@ -47,8 +80,7 @@ public class BuildingTest extends TestCase {
     public void testGetPercent() {
     }
 
-    public void testTestSetCapacity() {
-    }
+
 
     public void testIsInBuilding() {
     }
