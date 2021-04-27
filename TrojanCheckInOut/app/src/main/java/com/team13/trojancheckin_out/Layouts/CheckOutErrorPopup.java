b7c42865bd3b7280d4d95ec0b516c55148f13258
Liza Moody <emoody110@gmail.com>
2021-04-26 21:04:41 -0700

    check out error

---
+++ TrojanCheckInOut/app/src/main/java/com/team13/trojancheckin_out/Layouts/CheckOutErrorPopup.java
@@ -0,0 +1,45 @@
+package com.team13.trojancheckin_out.Layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.team13.trojancheckin_out.Accounts.QRCodeScanner;
+import com.team13.trojancheckin_out.Accounts.R;
+import com.team13.trojancheckin_out.Accounts.User;
+import com.team13.trojancheckin_out.UPC.Building;
+
+public class CheckOutErrorPopup extends AppCompatActivity {
+    private User user;
+    private Building match;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out_error_popup);
+
+        user = (User) getIntent().getSerializableExtra("PrevPageData");
+        match = (Building) getIntent().getSerializableExtra("building");
+
+        Button submit = (Button) findViewById(R.id.buttonSubmit);
+
+        //get building name for layout
+        TextView building = (TextView)findViewById(R.id.textView26);
+        building.setText(match.getName());
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to qr code scanner
+                Intent intent = new Intent(CheckOutErrorPopup.this, QRCodeScanner.class);
+                intent.putExtra("PrevPageData", user);
+                startActivity(intent);
+            }
+        });
+    }
+}
